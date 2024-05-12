e42fc65f4a6c83eef0add3b5727533630bfdcf26
shayan <30924971+shayan-fallah@users.noreply.github.com>
2024-05-13 00:49:24 +0330

    Update SpaceDodge.c

--- SpaceDodge/SpaceDodge.c
+++ SpaceDodge/SpaceDodge.c
@@ -54,17 +54,19 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
 
 /* USER CODE BEGIN PV */
 int rand_blk, relative_pos, block_pos;
-int i, j, k=21, r, test = 0;
+int i, j, k=21, r, test = 4, adc_in;
 int map[9][64];
-	
+int running = 1;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 void comm(char c)
 {
@@ -319,10 +321,12 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 	glcd_init();
   srand(time(NULL)); 
-
+	hadc1.Init.NbrOfConversion=1;
+	HAL_ADC_Init(&hadc1);
 
 //	int s = 60;
 //	comm(184);
@@ -333,9 +337,9 @@ int main(void)
 //		HAL_Delay(70);
 //	}
 
-	
 
 
+	draw_line(test);
 
 
   /* USER CODE END 2 */
@@ -347,6 +351,18 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+		
+		HAL_ADC_Start(&hadc1);
+		if(HAL_ADC_PollForConversion(&hadc1,100)==HAL_OK)
+		{
+			adc_in = HAL_ADC_GetValue(&hadc1);
+			if(test != (adc_in / 512))
+			{
+				test = adc_in / 512;
+			}
+		}
+		HAL_ADC_Stop(&hadc1);
+		HAL_Delay(20);
 		if(k > 20)
 		{
 			rand_blk = (rand() % (7 - 0 + 1)) + 0;
@@ -355,22 +371,25 @@ int main(void)
 				if(map[rand_blk][j] == 0) {map[rand_blk][j] = 60;break;}
 			}
 			k = 0;
-			draw_line(test++);
 		}
 		k++;
-		
-		for(i = 0; i < 8; i++)
+		if(running == 1)
 		{
-			for(j = 0; j < 64; j++)
+			for(i = 0; i < 8; i++)
 			{
-				if(map[i][j] != 0)
+				for(j = 0; j < 64; j++)
 				{
-					draw(map[i][j]++, i + 184);
-					if(map[i][j] > 194) map[i][j] = 0;
+					if(map[i][j] != 0)
+					{
+						draw(map[i][j]++, i + 184);
+						if(map[i][j] > 194) map[i][j] = 0;
+						if(i == test &&( map[i][j] == 140)) running = 0;
+						
+					}
 				}
 			}
 		}
-		HAL_Delay(50);
+		
   }
   /* USER CODE END 3 */
 }
@@ -383,6 +402,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -409,6 +429,59 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_9;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
